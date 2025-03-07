82319f20d2b979d423dabdb70df6d8dfd80711ed
Leah Victoria <systemgalaction@gmail.com>
2025-03-06 19:04:18 -0500

    learns something

--- AI Bot/Exec/AI.py
+++ AI Bot/Exec/AI.py
@@ -1,16 +1,21 @@
-goodwords = ["hi", "cool", "nice", "good", "perfect", "achievement", "bigger", "biggest", "goodest", "coolest", "hello", "nicest"]
+goodwords = ["hi", "cool", "nice", "good", "perfect", "achievement", "bigger", "biggest", "goodest", "coolest", "hello", "nicest", "lovely", "cute", "cutest", "thank", "thanks", "helped", "qpr", "advice", "wise", "wish", "wished", "wisdom", "acquintance", "enjoy", "happy", "happiness", "kind"]
 pronouns = ["I", "you", "he", "she", "they", "them", "we"]
-badwords = ["no", "ugh", "bad", "weird", "bad", "horrible", "terrible", "weirdest", "baddest"]
+badwords = ["no", "ugh", "bad", "weird", "bad", "horrible", "terrible", "weirdest", "baddest", "scared", "closeted", "scare", "love", "procrastination", "trauma", "stranger", "flirt", "romance", "alone", "killer", "dead", "deadly", "audacity",]
 
 mood = 0
-usersaid = input("Text: ").lower()
+while True:
+    usersaid = input("Text: ").lower()
 
 
-lenght = len(usersaid)
-word = 0
+    lenght = len(usersaid)
+    word = 0
 
-for word in goodwords:
-    if word in usersaid:
-        mood = mood + 1
+    for word in goodwords:
+     if word in usersaid:
+            mood = mood + 1
 
-print(mood)
+    for word in badwords:
+        if word in usersaid:
+            mood = mood - 1
+
+    print(mood)
